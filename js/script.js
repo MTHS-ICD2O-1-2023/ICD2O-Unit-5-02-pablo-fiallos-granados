d56ec3f06b61b9c4d9c3501ff74dbd9e54049804
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-04-24 18:49:37 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Pablo Fiallos
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+
+function enterClicked() {
+  // input
+  let randomNumber = Math.floor(Math.random() * 6) + 1
+  console.log(randomNumber)
+  let positiveClicked = document.getElementById('on-check').checked
+  // output
+  if (positiveClicked == true) {
+    document.getElementById("user-info").innerHTML =
+      randomNumber
+  } else {
+    document.getElementById("user-info").innerHTML =
+      -1 * randomNumber
+  }
+}
